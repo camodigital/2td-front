2814aef3a25899cd6905300c138c0028a4b51e04
André Santiago <andresantiagohellmann@gmail.com>
2021-03-22 17:40:24 -0300

    feat: add icons

---
+++ src/icons/icoArrowDown.tsx
@@ -0,0 +1,17 @@
+const icoArrowDown = () => (
+  <svg fill="none" viewBox="0 0 16 18">
+    <g clipPath="url(#clip0)">
+      <path
+        d="M7.5.667v10.03l-3.39-3.39-.72.72 4.25 4.25.36.343.36-.344 4.25-4.25-.72-.719-3.39 3.39V.668h-1z"
+        fill="#87909C"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0">
+        <path fill="#fff" d="M0 0h16v17.333H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+)
+
+export default icoArrowDown
---
+++ src/icons/icoArrowLeft.tsx
@@ -0,0 +1,10 @@
+const icoArrowLeft = () => (
+  <svg fill="none" viewBox="0 0 12 20">
+    <path
+      d="M11.036 10.99L2.4 19.587A1.406 1.406 0 010 18.6a1.4 1.4 0 01.41-.992l7.642-7.604L.41 2.392A1.401 1.401 0 011.407 0c.372 0 .73.149.993.412l8.636 8.591a1.397 1.397 0 010 1.98v.006z"
+      fill="#FAFAFA"
+    />
+  </svg>
+)
+
+export default icoArrowLeft
