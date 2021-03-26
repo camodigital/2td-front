211f589fff638aab73c98d952acabe31b6bc6e61
André Santiago <andresantiagohellmann@gmail.com>
2021-03-26 17:29:04 -0300

    chore: create sec/SecContact

---
+++ src/sections/SecContact/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const SecContact = () => (
+  <S.Wrapper>
+    <h1>SecContact</h1>
+  </S.Wrapper>
+)
+
+export default SecContact
--- src/templates/TheHome/index.tsx
+++ src/templates/TheHome/index.tsx
@@ -4,6 +4,7 @@ import SecIntro from 'sections/SecIntro'
 import SecProducts from 'sections/SecProducts'
 import SecAbout from 'sections/SecAbout'
 import SecClients from 'sections/SecClients'
+import SecContact from 'sections/SecContact'
 
 const TheHome = () => (
   <S.Wrapper>
@@ -11,6 +12,7 @@ const TheHome = () => (
     <SecProducts />
     <SecAbout />
     <SecClients />
+    <SecContact />
   </S.Wrapper>
 )
 
