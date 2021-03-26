02080f9687dd1cd4935623e35b97229cb9575b44
André Santiago <andresantiagohellmann@gmail.com>
2021-03-26 16:29:34 -0300

    fix: remove stories from cleanslide

--- src/components/CleanSlider/CleanSlide/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import CleanSlide from '.'
-
-export default {
-  title: 'CleanSlide',
-  component: CleanSlide
-} as Meta
-
-export const Default: Story = () => <CleanSlide />
--- src/components/CleanSlider/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import CleanSlider from '.'
-
-export default {
-  title: 'CleanSlider',
-  component: CleanSlider
-} as Meta
-
-export const Default: Story = () => <CleanSlider />
