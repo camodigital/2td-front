da5737dfda62f562e227d367f5cf3608c6d575e2
André Santiago <andresantiagohellmann@gmail.com>
2021-03-30 10:51:43 -0300

    fix: remove storie from ThePage/Template

--- src/templates/ThePage/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import ThePage from '.'
-
-export default {
-  title: 'ThePage',
-  component: ThePage
-} as Meta
-
-export const Default: Story = () => <ThePage />
