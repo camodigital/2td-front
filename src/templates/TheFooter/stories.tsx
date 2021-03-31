8101ee14194389cff703d8788d578e1ddd1d8f63
André Santiago <andresantiagohellmann@gmail.com>
2021-03-30 23:30:24 -0300

    fix: remove stories from templates

--- src/templates/TheFooter/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import TheHeader from '.'
-
-export default {
-  title: 'TheHeader',
-  component: TheHeader
-} as Meta
-
-export const Default: Story = () => <TheHeader />
--- src/templates/TheHome/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import TheHome from '.'
-
-export default {
-  title: 'TheHome',
-  component: TheHome
-} as Meta
-
-export const Default: Story = () => <TheHome />
